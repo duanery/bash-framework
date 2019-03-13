69522eedf2d1593e3f85c1d613538920a924acf6
duane <42316014+duanery@users.noreply.github.com>
2019-03-13 16:35:44 +0800

    change

--- psql.h
+++ psql.h
@@ -8,15 +8,11 @@ define __PSQL_H__
 #------------------------------------------------------------------------
 if which psql >/dev/null 2>&1; then
 
-__PSQL=`which psql`
-__PSQLPORT=$__srvport_psql
-__PSQLIP=$__srvip
-
-
-#psql -U postgres -t -A -F, -q -c "select * from tbl_vm"
+#psql -t -A -F, -q -c "select * from tbl_vm"
 psql() {
-    $__PSQL -h $__PSQLIP -p $__PSQLPORT -d cloudbox -U postgres -t -A -F, -q -c "$*"
+    psql -t -A -F, -q -c "$*"
 }
+
 #psqlfield "$Q" 1
 psqlfield() {
     awk -F ',' -v K=$2 '{print $K}' <<< "$1"
