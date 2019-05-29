980a9427c8ac18b5c9dfa4b685d7ed4783b25196
duanery <corcpp@foxmail.com>
2019-05-30 01:34:59 +0800

    add co in co

--- test_coproc.sh
+++ test_coproc.sh
@@ -8,23 +8,39 @@ include core.h
 include coproc.h
 source options.h
 
+
+#=========================
+# TEST
 costart TEST << "EOF"
     while read line
     do
         echo $line
     done
 EOF
-
 cowrite TEST <<< "hello"
 coread TEST
 cowrite TEST <<< "world!"
 coread TEST
-
 cokill TEST
 
 
+#=========================
+# PING
 costart PING << "EOF"
+    ping -c 4 127.0.0.1
+EOF
+cocat PING
+
+
+#=========================
+# CO IN CO
+inco() {
+    costart HELP << "EOF"
     ping 127.0.0.1
 EOF
-
-cocat PING
+}
+costart COCO << "EOF"
+    inco
+    cocat HELP
+EOF
+cocat COCO
